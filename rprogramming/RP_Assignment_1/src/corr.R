f73b8ac186909cb8315408690485bf44aa6aeda0
Meredith Winchell <meredithwwinchell@gmail.com>
2014-08-18 22:30:11 -0400

    Finally finished corr.R

--- rprogramming/RP_Assignment_1/src/corr.R
+++ rprogramming/RP_Assignment_1/src/corr.R
@@ -1,18 +1,13 @@
 corr <- function (directory, threshold = 0){
-  files <- dir(directory, full.names=TRUE)
-  numeric.vector <- numeric()
-  csvs <- data.frame() 
-  for (i in 1:332) {
-    csv <- read.csv(files[i])
-    csvs <- rbind (csvs, csv)
+  files <- dir(directory, full.names=TRUE) #get a list of files in the directory
+  numeric_vector <- numeric()
+  for (i in files) { #for each file
+    data_frame <- read.csv(i) #read it
+    nobs <- sum(complete.cases(data_frame)) #find number of complete observations for data frame
+    if (nobs > threshold) {#see if it meets a threshold
+      correlation <- cor(data_frame$sulfate, data_frame$nitrate, use = "complete.obs") #if so, calculate the correlation
+      numeric_vector <- c(numeric_vector, correlation) #and add the correlation to a vector
+    }
   }
-  complete.cases.files <- complete("specdata", 1:332)
-  for (nobs in complete.cases.files["nobs"]) {
-    nobs.above.threshold <- nobs > threshold
-      if (nobs.above.threshold[ ] == TRUE) {
-        correlation <- cor(csvs$sulfate, csvs$nitrate, use = "complete.obs")
-        rbind(numeric.vector, correlation)
-      }
-  }
-  print(numeric.vector)
+  return(numeric_vector)
 }
